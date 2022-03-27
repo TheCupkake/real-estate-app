a227047d5d253a9fff3ba59ecb25b3bfa75f13fc
Eyad Abu-Zaid <33122334+TheCupkake@users.noreply.github.com>
2022-03-27 02:44:04 +0200

    finished project

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import {Flex, Box, Text, Button} from '@chakra-ui/react';
 import {baseUrl, fetchApi} from '../utils/fetchApi';
+import Property from '../components/Property'
 
 const Banner = ({purpose, title1, title2, desc1, desc2, linkName, buttonText, imageUrl}) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
@@ -20,7 +21,6 @@ const Banner = ({purpose, title1, title2, desc1, desc2, linkName, buttonText, im
 export default function Home({propertiesForSale, propertiesForRent}) {
   return (
     <Box>
-      <h1>Hello World</h1>
       <Banner 
         purpose="RENT A HOME "
         title1="Rental Homes For"
@@ -29,11 +29,11 @@ export default function Home({propertiesForSale, propertiesForRent}) {
         desc2="and more"
         buttonText="Explore Renting"
         linkName="/search?purpose=for-rent"
-        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
 
       />
       <Flex flexWrap="wrap">
-
+        {propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
       </Flex>
       <Banner 
         purpose="BUY A HOME "
@@ -43,10 +43,13 @@ export default function Home({propertiesForSale, propertiesForRent}) {
         desc2="and more"
         buttonText="Explore Buying"
         linkName="/search?purpose=for-sale"
-        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+        imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
         
       
       />
+      <Flex flexWrap="wrap">
+        {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
+      </Flex>
     </Box>
   )
 }
